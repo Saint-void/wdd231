56e39de4ebb4f1fcbcdc148639077496fb3e204e
saint-void <sogolodonald11@gmail.com>
2026-03-26 21:16:00 +0100

    ddsw

---
+++ scripts/modules.mjs
@@ -0,0 +1,33 @@
+import byuiCourse from "./course.mjs";
+import { setSectionSelection } from "./sections.mjs";
+import { setTitle, renderSections } from "./output.mjs";
+
+// Populate page on load
+setTitle(byuiCourse);
+setSectionSelection(byuiCourse.sections);
+renderSections(byuiCourse.sections);
+
+// Event listeners
+document
+  .querySelector("#enrollStudent")
+  .addEventListener("click", function () {
+    const sectionNum = Number(
+      document.querySelector("#sectionNumber").value
+    );
+
+    byuiCourse.changeEnrollment(sectionNum);
+
+    renderSections(byuiCourse.sections);
+  });
+
+document
+  .querySelector("#dropStudent")
+  .addEventListener("click", function () {
+    const sectionNum = Number(
+      document.querySelector("#sectionNumber").value
+    );
+
+    byuiCourse.changeEnrollment(sectionNum, false);
+
+    renderSections(byuiCourse.sections);
+  });
